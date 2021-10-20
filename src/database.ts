df8f68579772f811b5050b30b2cdc88cb07e93ae
Manzanit0 <unico.javier@gmail.com>
2021-10-20 22:47:49 +0200

    Refactor repository layer

---
+++ src/database.ts
@@ -0,0 +1,45 @@
+import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
+import { QueryObjectResult } from "https://deno.land/x/postgres@v0.11.3/query/query.ts";
+
+export function newClient() {
+  return new Client({
+    user: Deno.env.get("PGUSER"),
+    password: Deno.env.get("PGPASSWORD"),
+    database: Deno.env.get("PGDATABASE"),
+    hostname: Deno.env.get("PGHOST"),
+    port: Deno.env.get("PGPORT"),
+  });
+}
+
+export async function runQuery<T>(query: string, ...args: string[]) {
+  const client = newClient();
+  await client.connect();
+  const result = await client.queryObject<T>(query, ...args);
+  client.end();
+  return result;
+}
+
+export function unwrapMany<T, K>(mapper: (x: T) => K, r: QueryObjectResult<T>) {
+  return unwrapRaw(r).map(mapper);
+}
+
+export function unwrapOne<T, K>(mapper: (x: T) => K, r: QueryObjectResult<T>): K | null {
+  const unwrapped = unwrapMany(mapper, r);
+  if (unwrapped.length > 1) {
+    throw new Error(`${unwrapped.length} records returned from query`);
+  }
+
+  if (unwrapped === []) {
+    return null;
+  }
+
+  return unwrapped[0];
+}
+
+export function unwrapRaw<T>(r: QueryObjectResult<T>): T[] {
+  if (r.rowCount === 0) {
+    return [];
+  }
+
+  return r.rows;
+}
--- src/repository.ts
+++ src/repository.ts
@@ -1,48 +1,26 @@
 // deno-lint-ignore-file camelcase
-import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
+import * as R from "https://raw.githubusercontent.com/selfrefactor/rambda/master/dist/rambda.esm.js";
+import { runQuery, unwrapMany, unwrapOne } from "./database.ts";
 
-const client = new Client({
-  user: Deno.env.get("PGUSER"),
-  password: Deno.env.get("PGPASSWORD"),
-  database: Deno.env.get("PGDATABASE"),
-  hostname: Deno.env.get("PGHOST"),
-  port: Deno.env.get("PGPORT"),
-});
+// User Repository
 
 export type User = {
   id: string;
   telegram_chat_id: string;
 };
 
-export async function findUser(telegramId: string) {
-  const result = await runQuery<User>(
-    "SELECT id, telegram_chat_id FROM users WHERE telegram_chat_id = $1",
-    telegramId,
-  );
-
-  if (result.rowCount == 0) {
-    return null;
-  }
-
-  if (result.rowCount && result.rowCount > 1) {
-    throw new Error('found multiple contacts by the same "telegram_chat_id"');
-  }
-
-  return result.rows[0];
+export function findUser(telegramId: string): Promise<User | null> {
+  const query = `SELECT id, telegram_chat_id FROM users WHERE telegram_chat_id = '${telegramId}'`;
+  return runQuery<User>(query).then(unwrapOneUser);
 }
 
-export async function createUser({ telegramId }: { telegramId: string }) {
-  const result = await runQuery<User>(
-    "INSERT INTO users (telegram_chat_id) VALUES ($1) RETURNING id, telegram_chat_id",
-    telegramId,
-  );
-
-  if (result.rowCount != 1 || !result.rows[0]) {
-    throw new Error("unable to create user");
-  }
-
-  return result.rows[0];
+export function createUser({ telegramId }: { telegramId: string }): Promise<User> {
+  const query = `INSERT INTO users (telegram_chat_id) VALUES ('${telegramId}') RETURNING id, telegram_chat_id`;
+  console.log(query);
+  return runQuery<User>(query).then(unwrapOneUser);
 }
+
+// Location Repository
 
 export type Coordinates = { latitude: number; longitude: number };
 
@@ -68,79 +46,31 @@ type CreateLocationParams = {
   positionstack?: unknown;
 };
 
-export async function createUserLocation(
-  params: CreateLocationParams,
-): Promise<UserLocation> {
-  const result = await runQuery<DbUserLocation>(
-    "INSERT INTO user_locations (user_id, name, coordinates, positionstack_response) VALUES ($1, $2, $3, $4) RETURNING id, user_id, name, coordinates",
-    params.user_id,
-    params.name || "",
-    encodeCoordinates(params.coordinates),
-    JSON.stringify(params.positionstack),
-  );
+export function createUserLocation(params: CreateLocationParams): Promise<UserLocation> {
+  const name = params.name || "";
+  const positionstack = JSON.stringify(params.positionstack);
+  const coordinates = encodeCoordinates(params.coordinates);
 
-  if (result.rowCount != 1 || !result.rows[0]) {
-    throw new Error("unable to create user_location");
-  }
+  const query = `
+     INSERT INTO user_locations (user_id, name, coordinates, positionstack_response)
+     VALUES ('${params.user_id}', '${name}', ${coordinates}, '${positionstack}')
+     RETURNING id, user_id, name, coordinates`;
 
-  return {
-    ...result.rows[0],
-    coordinates: decodeCoordinates(result.rows[0].coordinates),
-  };
+  return runQuery<DbUserLocation>(query).then(unwrapOneLocation);
 }
 
-export async function findUserLocation(name: string, userId: string): Promise<UserLocation | null> {
-  const result = await runQuery<DbUserLocation>(
-    "SELECT id, user_id, name, coordinates FROM user_locations WHERE LOWER(name) = LOWER($1) AND user_id = $2",
-    name,
-    userId,
-  );
+export function findUserLocation(name: string, userId: string): Promise<UserLocation | null> {
+  const query = `SELECT id, user_id, name, coordinates
+                 FROM user_locations
+                 WHERE LOWER(name) = LOWER('${name}')
+                 AND user_id = '${userId}'`;
 
-  if (!result) {
-    throw new Error("no result returned from query");
-  }
-
-  if (result.rowCount == 0) {
-    return null;
-  }
-
-  if (result.rowCount && result.rowCount > 1) {
-    throw new Error('found multiple locations with the same name for the same user');
-  }
-
-  return {
-    ...result.rows[0],
-    coordinates: decodeCoordinates(result.rows[0].coordinates),
-  };
+  return runQuery<DbUserLocation>(query).then(unwrapOneLocation);
 }
 
-export async function listUserLocations(
-  userId: string,
-): Promise<UserLocation[]> {
-  const result = await runQuery<DbUserLocation>(
-    "SELECT id, user_id, name, coordinates FROM user_locations WHERE user_id = $1",
-    userId,
-  );
-
-  if (!result) {
-    throw new Error("no result returned from query");
-  }
-
-  if (result.rowCount == 0) {
-    return [];
-  }
-
-  return result.rows.map((x) => ({
-    ...x,
-    coordinates: decodeCoordinates(x.coordinates),
-  }));
-}
-
-async function runQuery<T>(query: string, ...args: string[]) {
-  await client.connect();
-  const result = await client.queryObject<T>(query, ...args);
-  client.end();
-  return result;
+export function listUserLocations(userId: string): Promise<UserLocation[]> {
+  const query = `SELECT id, user_id, name, coordinates FROM user_locations WHERE user_id = '${userId}'`;
+  return runQuery<DbUserLocation>(query).then(unwrapLocations);
 }
 
 function decodeCoordinates(coordinates: string): Coordinates {
@@ -164,3 +94,14 @@ function decodeCoordinates(coordinates: string): Coordinates {
 function encodeCoordinates(coordinates: Coordinates) {
   return `(${coordinates.latitude}, ${coordinates.longitude})`;
 }
+
+const toUserLocation = (x: DbUserLocation): UserLocation => ({
+  ...x,
+  coordinates: decodeCoordinates(x.coordinates),
+});
+
+const unwrapOneUser = R.curry(unwrapOne)(R.identity);
+
+const unwrapLocations = R.curry(unwrapMany)(toUserLocation);
+
+const unwrapOneLocation = R.curry(unwrapOne)(toUserLocation);
