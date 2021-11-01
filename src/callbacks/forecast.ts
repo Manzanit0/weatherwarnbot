fdc406a6c36cc520a657023b192bea8147b1b229
Manzanit0 <unico.javier@gmail.com>
2021-11-01 19:48:26 +0100

    Refactor forecasts into usecase abstraction

---
+++ src/callbacks/forecast.ts
@@ -0,0 +1,43 @@
+import { buildForecastMessage, Day, fetchWeatherByCoordinates } from "../forecast.ts";
+import { AuthenticatedContext } from "../middleware.ts";
+import { findLocationById } from "../repository.ts";
+import { answerCallbackQuery, sendMessage, TelegramRequestBody } from "../telegram.ts";
+
+const callbackDataKey = "forecast:";
+
+const isForecastCallback = (body: TelegramRequestBody) => body.callback_query?.data.includes(callbackDataKey) ?? false;
+
+async function handleBookmarkLocationCallback(ctx: AuthenticatedContext) {
+  if (!isForecastCallback(ctx.payload)) {
+    throw new Error("no valid forecast callback");
+  }
+
+  const data = ctx.payload.callback_query!.data;
+
+  const locationId = data.split(":")[2];
+  if (!locationId) {
+    throw new Error("Unable to extract location name from callback_query");
+  }
+
+  // To receive this kind of payload, the location must have been bookmarked.
+  const location = await findLocationById(locationId);
+  if (!location) {
+    throw new Error("received forecast:now callback for a location that doesn't exist");
+  }
+
+  const forecastDay = data.includes("forecast:tomorrow") ? Day.TOMORROW : Day.TODAY;
+  const forecast = await fetchWeatherByCoordinates(
+    location.coordinates.latitude,
+    location.coordinates.longitude,
+    forecastDay,
+  );
+
+  await answerCallbackQuery(ctx.payload, `Fetching weather for ${location.name || "location"}`);
+  const message = buildForecastMessage({ ...forecast, location: location.name! });
+  sendMessage(ctx.user.telegram_chat_id, message);
+}
+
+export default {
+  isValid: isForecastCallback,
+  handle: handleBookmarkLocationCallback,
+};
--- src/telegram_controller.ts
+++ src/telegram_controller.ts
@@ -1,24 +1,19 @@
 import { CallbackUsecase } from "./callbacks/callbackUsecase.ts";
 import bookmarkLocationUsecase from "./callbacks/bookmarkLocation.ts";
 import settingsUsecase from "./callbacks/settings.ts";
+import forecastUsecase from "./callbacks/forecast.ts";
 
 import { buildForecastMessage, Day, fetchWeatherByCoordinates, fetchWeatherByName } from "./forecast.ts";
 import { AuthenticatedContext } from "./middleware.ts";
-import { findLocationById, listLocations } from "./repository.ts";
+import { listLocations } from "./repository.ts";
 import {
   answerCallbackQuery,
   parseCommand,
   response,
-  sendMessage,
   withForecastRequestInlineMenu,
   withLocationInlineMenu,
   withSettingsInlineMenu,
 } from "./telegram.ts";
-
-const usecases: CallbackUsecase[] = [
-  bookmarkLocationUsecase,
-  settingsUsecase,
-];
 
 export async function handleCallback(ctx: AuthenticatedContext) {
   const data = ctx.payload.callback_query?.data;
@@ -26,30 +21,15 @@ export async function handleCallback(ctx: AuthenticatedContext) {
     throw new Error("telegram payload missing callback_query");
   }
 
-  usecases.forEach((x) => x.isValid(ctx.payload) ? x.handle(ctx) : null);
+  const usecases: CallbackUsecase[] = [
+    bookmarkLocationUsecase,
+    settingsUsecase,
+    forecastUsecase,
+  ];
 
-  if (data.includes("forecast:")) {
-    const locationId = data.split(":")[2];
-    if (!locationId) {
-      throw new Error("Unable to extract location name from callback_query");
-    }
-
-    // To receive this kind of payload, the location must have been bookmarked.
-    const location = await findLocationById(locationId);
-    if (!location) {
-      throw new Error("received forecast:now callback for a location that doesn't exist");
-    }
-
-    const forecastDay = data.includes("forecast:tomorrow") ? Day.TOMORROW : Day.TODAY;
-    const forecast = await fetchWeatherByCoordinates(
-      location.coordinates.latitude,
-      location.coordinates.longitude,
-      forecastDay,
-    );
-
-    await answerCallbackQuery(ctx.payload, `Fetching weather for ${location.name || "location"}`);
-    const message = buildForecastMessage({ ...forecast, location: location.name! });
-    sendMessage(ctx.user.telegram_chat_id, message);
+  const usecase = usecases.find((x) => x.isValid(ctx.payload));
+  if (usecase) {
+    await usecase.handle(ctx);
   } else {
     await answerCallbackQuery(ctx.payload, `received ${data} callback`);
   }
